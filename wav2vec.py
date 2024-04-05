0cfea2b3cb647356759db0596765e806acdfdc70
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-04-05 20:01:56 +0000

    feat: add wav2vec model

---
+++ wav2vec.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import Wav2Vec2ForCTC, AutoProcessor, pipeline
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "facebook/seamless-m4t-v2-large"
+
+model = Wav2Vec2ForCTC.from_pretrained(
+    model_id, torch_dtype=torch_dtype
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=30,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+result = pipe("voice_01.wav")
+print(result["text"])
